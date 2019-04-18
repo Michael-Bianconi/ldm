89a5e99d7176dc48e7d2b5be32a30275f4ae9b43
Michael-Bianconi <michael.a.bianconi@gmail.com>
2019-04-18 11:43:16 -0400

    Sentence header

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ldm
 Logic Derivation Machine parses logic sentences and performs derivations
+
+# Rules
+
+Sentences may be either <i>atomic</i> or <i>COMPOUND</i>. Atomic sentences
+do not contain any operators, and are represented with characters (e.g. a).
+
+Compound sentences hold (usually) two sentences connected by an operator.
+The inner sentences may themselves be atomic or compound. In the case of
+a unary operator (e.g. negation), only the <i>left</i> inner sentence is
+used.
---
+++ include/sentence.h
@@ -0,0 +1,137 @@
+/**
+ * @author Michael Bianconi
+ * @since 04-18-2019
+ */
+
+#ifndef SENTENCE_H
+#define SENTENCE_H
+
+/// ===========================================================================
+/// Structure declarations
+/// ===========================================================================
+
+struct Sentence_s;
+
+/// ===========================================================================
+/// Enum definitions
+/// ===========================================================================
+
+/**
+ * Sentences have two different types: atomic and compound. Atomic sentences
+ * contain only one component, a variable. Compound sentences contain
+ * two components, each of which may themselves be either atomic or compound.
+ * Note: there is a special type of Sentence that has the negation operator.
+ * this unary operator is the only operator that will form a compound sentence
+ * with only one inner sentence.
+ */
+enum SentenceType
+{
+	ATOMIC,
+	COMPOUND
+};
+
+enum SentenceOperator
+{
+	NO_OP,
+	AND,
+	OR,
+	NEGATION,
+	MATERIAL_CONDITIONAL,
+	MATERIAL_BICONDITIONAL
+};
+
+/// ===========================================================================
+/// Union definitions
+/// ===========================================================================
+
+/**
+ * Holds either a variable or a sentence pointer. Atomic sentences hold
+ * variables, compound sentences hold sentence pointers.
+ */
+union SentenceBuffer
+{
+	char variable;
+	struct Sentence_s*;
+};
+
+/// ===========================================================================
+/// Structure definitions
+/// ===========================================================================
+
+/**
+ * Note: sentences that only make use of one buffer (atomic sentences and
+ * negated sentences) will always only use the left buffer.
+ */
+struct Sentence_s
+{
+	enum SentenceType type;
+	enum SentenceOperator op;
+	union SentenceBuffer left;
+	union SentenceBuffer right;
+};
+
+/// ===========================================================================
+/// Typedefs
+/// ===========================================================================
+
+typedef enum SentenceType SentenceType;
+typedef enum SentenceOperator SentenceOperator;
+typedef union SentenceBuffer SentenceBuffer;
+typedef struct Sentence_s* Sentence;
+
+/// ===========================================================================
+/// Function declarations
+/// ===========================================================================
+
+/**
+ * Constructs an atomic sentence with no operator and a single variable
+ * in the left buffer.
+ *
+ * @param var Variable to set.
+ * @return Returns a malloc'd Sentence.
+ */
+Sentence Sentence_createAtomic(char var);
+
+/**
+ * Creates a compound sentence.
+ *
+ * @param op Operator to use. Should not be NEGATION.
+ * @param left The sentence to store in the left buffer.
+ * @param right The sentence to store in the right buffer.
+ * @return Returns a malloc'd sentence.
+ */
+Sentence Sentence_createCompound(
+	SentenceOperator op,
+	Sentence left,
+	Sentence right);
+
+/**
+ * Constructs a negated sentence, similar to an atomic sentence but
+ * with a sentence instead of a variable. Note: this will
+ * create a <i>new</i> sentence without altering the given one.
+ *
+ * @param toNegate The sentence to negate.
+ * @return Returns a new negated sentence with the given sentence
+ *         in the left buffer.
+ */
+Sentence Sentence_createNegated(Sentence toNegate);
+
+/**
+ * Frees the given sentence but <i>not</i> it's left and right
+ * buffers.
+ *
+ * @param sentence Sentence to free.
+ */
+void Sentence_free(Sentence sentence);
+
+/**
+ * Frees the given sentence <i>and</i> the left and
+ * right buffers, recursively.
+ *
+ * @param sentence Root sentence to free.
+ */
+void Sentence_freeRecursive(Sentence sentence);
+
+
+
+#endif;
